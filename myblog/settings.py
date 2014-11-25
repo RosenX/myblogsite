3157fd6f12ca4a7eb57328ff72d3184f8561501a
RosenX <Rosen9212@gmail.com>
2014-11-25 23:00:40 +0800

    修改了数据库用户名

--- myblog/settings.py
+++ myblog/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'myblog', # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
+        'USER': 'Rosen',                      # Not used with sqlite3.
         'PASSWORD': '921204ls',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
